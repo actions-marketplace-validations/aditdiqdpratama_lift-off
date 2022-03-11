83d8c2ded5ae08fa2f375a28737e4b31dcb1ef1d
Adityo Pratama <diqdqd@gmail.com>
2022-03-11 20:32:03 +0700

    feat: fetch action

---
+++ index.js
@@ -0,0 +1,37 @@
+const core = require('@actions/core');
+
+try {
+    var myHeaders = new Headers();
+    myHeaders.append("Accept", "application/json");
+    // `token` input defined in action metadata file
+    myHeaders.append("Authorization", `Bearer ${core.getInput('token')}`);
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+        "content": {
+            "className": "ChatMessage.Text",
+            // `message` input defined in action metadata file
+            "text": core.getInput('message'),
+        },
+        // `channel` input defined in action metadata file
+        "channel": core.getInput('channel')
+    });
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+
+    // `base-url` input defined in action metadata file
+    fetch(`https://${core.getInput('base-url')}/api/http/chats/messages/send-message`, requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result));
+
+    const time = (new Date()).toTimeString();
+    // `time` output defined in action metadata file
+    core.setOutput("time", time);
+} catch (error) {
+    core.setFailed(error.message);
+}
